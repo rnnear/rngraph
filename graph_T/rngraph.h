94f883b41d9f5acca8a5e718e5f41a7d5063aba1
rnnear <rngood1993@gmail.com>
2017-06-14 17:26:28 +0800

    update6_14
    complete rnDijkstra();
    complete rnPrim();
    add some methods about undirected graph;

--- graph_T/rngraph.h
+++ graph_T/rngraph.h
@@ -1,10 +1,17 @@
 // directed acyclic graph
+/* This is a class template of graph(DAG)
+ * Condition: Weight type must reload operation< and operation+;(required by rngraph::rnDijkstra(...))
+ *			  Vertex type must reload operation<<;(required by rngraph::Display(...), rngraph::DisplayU(...) and rnpath::Display(...))
+ * Tips: Vertex type is set to string as default;
+ *		 You'd better reload operation< of vertex type;(promote efficient)
+ */
 #include<iostream>
 #include<fstream>
 #include<string>
 #include<vector>
 #include<forward_list>
 #include<map>
+#include<unordered_set>
 #include<queue>
 #include<utility>
 #include<tuple>
@@ -25,7 +32,7 @@ template<typename T1, typename T2> class rnpath;
 template<typename T1, typename T2> class rnedge;
 template<typename T1, typename T2> class rngraph;
 
-//auxiliary class template, including 'rnedge', 'rnpath', 'rncompare', 'rncompare2'
+//auxiliary class template and function template, including 'rnedge', 'rnpath', 'rncompare', 'rncompare2', 'rncompare3'
 template<typename Value_T, typename ID_T>
 class rnedge
 {
@@ -47,38 +54,13 @@ private:
 	ID_T to;
 	Value_T weight;
 };
-/*
- * template<typename Value_T, typename ID_T>
- *class rnmark_node
- *{
- *public:
- *	using size_type = rnposition_type;
- *	rnmark_node() = default;
- *	rnmark_node(const size_type rnlast_n, const size_type rnthis_n, const Value_T& to_this_w): last_node(rnlast_n),
- *				this_node(rnthis_n), to_this_weight(to_this_w){};
- *	size_type get_last() const
- *	{
- *		return last_node; 
- *	}
- *	size_type get_this() const
- *	{
- *		return this_node;
- *	}
- *	Value_T get_weight() const
- *	{
- *		return to_this_weight;
- *	}
- *private:
- *	size_type last_node, this_node;  //record link information by node position
- *	Value_T to_this_weight;
- *};
- */
 template<typename Value_T, typename ID_T>
 class rnpath
 {
 public:
 	rnpath() = default;
-	rnpath(std::shared_ptr<std::forward_list<ID_T>>& rnflist_ptr, const Value_T& rnsum_w):path_flist_ptr(rnflist_ptr), sum_weight(rnsum_w){}
+	rnpath(std::shared_ptr<std::forward_list<ID_T>>& rnflist_ptr, const Value_T& rnsum_w):
+		path_flist_ptr(rnflist_ptr), sum_weight(rnsum_w){}
 	bool IsValid() const
 	{
 		if(!(path_flist_ptr))
@@ -113,7 +95,7 @@ struct rncompare
 {
 	bool operator()(const std::pair<rnposition_type, Value_T>& rnleft, const std::pair<rnposition_type, Value_T>& rnright)
 	{
-		return rngraph<Value_T, ID_T>::get_value(rnleft.second) > rngraph<Value_T, ID_T>::get_value(rnright.second);
+		return !(rngraph<Value_T, ID_T>::get_value(rnleft.second) < rngraph<Value_T, ID_T>::get_value(rnright.second));
 	}
 };
 template<typename Value_T, typename ID_T>
@@ -122,9 +104,16 @@ struct rncompare2
 	bool operator()(const std::tuple<rnposition_type, rnposition_type, Value_T, Value_T> rnleft, 
 			const std::tuple<rnposition_type, rnposition_type, Value_T, Value_T> rnright)
 	{
-		return rngraph<Value_T, ID_T>::get_value(std::get<2>(rnleft)) > rngraph<Value_T, ID_T>::get_value(std::get<2>(rnright));
+		return !(rngraph<Value_T, ID_T>::get_value(std::get<2>(rnleft)) < rngraph<Value_T, ID_T>::get_value(std::get<2>(rnright)));
 	}
 };
+//function template
+template<typename Value_T, typename ID_T>
+bool rncompare3(const std::tuple<rnposition_type, rnposition_type, Value_T>& rnleft, 
+				const std::tuple<rnposition_type, rnposition_type, Value_T>& rnright)
+{
+	return !(rngraph<Value_T, ID_T>::get_value(std::get<2>(rnleft)) < rngraph<Value_T, ID_T>::get_value(std::get<2>(rnright)));
+}
 //class template rngraph
 /* info about DS
  * graph: vector(pair(ID_T, ptr->vector(pair(pos, value_T))))
@@ -134,18 +123,19 @@ struct rncompare2
 template<typename Value_T, typename ID_T = std::string>
 class rngraph
 {
-	//friend class rnedge<Value_T, ID_T>;
 	friend class rncompare<Value_T, ID_T>;
 	friend class rncompare2<Value_T, ID_T>;
+	friend bool rncompare3<Value_T, ID_T>(const std::tuple<rnposition_type, rnposition_type, Value_T>&, 
+				const std::tuple<rnposition_type, rnposition_type, Value_T>&);
 	static bool support_less_operator;
 	static void initialize()
 	{
 		support_less_operator = HasOperatorLess<ID_T>::value;
 	}
 	static Value_T get_value(const Value_T& rnweight)  //get weight from object of Value_T, 
-	{						  //getting value of Value_T object directly is accepted as default
-		return rnweight;
-	}
+	{												   //getting value of Value_T object directly is accepted as default
+		return rnweight;                               //You can process the true value of weight, and this function will be called in the rncompare*(...) 
+	}												   //and GetWeight(...)
 public: 
 	typedef rnposition_type size_type;
 	rngraph() 
@@ -154,14 +144,8 @@ public:
 	}
 	rngraph(std::istream& rninput_file): rngraph() // construct graph by a formatted txt file
 	{
-#ifndef DEBUG
-int i = 0;
-#endif
 		while(rninput_file)
 		{
-#ifndef DEBUG
-//std::cout<<++i<<std::endl;
-#endif
 			ID_T rnfrom, rnto;
 			Value_T rnvalue;
 			rninput_file>>rnfrom>>rnto>>rnvalue;
@@ -174,10 +158,26 @@ std::cout<<"from:"<<rnfrom<<" to:"<<rnto<<std::endl;
 		}
 		rninput_file.clear();
 	}
-	~rngraph(){}
+	rngraph(std::istream& rninput_file, int undirected): rngraph() // construct graph by a formatted txt file
+	{
+		while(rninput_file)
+		{
+			ID_T rnfrom, rnto;
+			Value_T rnvalue;
+			rninput_file>>rnfrom>>rnto>>rnvalue;
+			if(rnfrom == "")
+				continue;
+#ifndef DEBUG
+			std::cout<<"from:"<<rnfrom<<" to:"<<rnto<<std::endl;
+#endif
+			PushEdgeU(rnfrom, rnto, rnvalue);
+		}
+		rninput_file.clear();
+	}
 
+	~rngraph() = default;
 //add net edge
-	bool PushEdge(const ID_T& rnfrom_id, const ID_T& rnto_id, const Value_T& rnval) //iterator exit
+	bool PushEdge(const ID_T& rnfrom_id, const ID_T& rnto_id, const Value_T& rnval) //
 	{
 		if(rnfrom_id == rnto_id)
 		{
@@ -210,7 +210,7 @@ std::cout<<"from:"<<rnfrom<<" to:"<<rnto<<std::endl;
 #endif
 
 			rnto_pos = graph.size() - 1;
-			add_pos(rnto_id, rnto_pos);         //add to ID-to-pos list
+			add_pos(rnto_id, rnto_pos);         //add to ID-postion list
 		}
 
 		auto rnfrom_pos = get_pos(rnfrom_id);
@@ -219,7 +219,7 @@ std::cout<<"from:"<<rnfrom<<" to:"<<rnto<<std::endl;
 			auto rnptr = std::make_shared<std::vector<std::pair<size_type, Value_T>>>();
 			graph.push_back({rnfrom_id, rnptr});
 			auto rnpos = graph.size() - 1;
-			add_pos(rnfrom_id, rnpos);             //add to ID-to-pos list
+			add_pos(rnfrom_id, rnpos);             //add to ID-postion list
 			rnptr->push_back({rnto_pos, rnval});
 #ifndef DEBUG1
 			std::cout<<"3"<<std::endl;
@@ -239,8 +239,18 @@ std::cout<<"from:"<<rnfrom<<" to:"<<rnto<<std::endl;
 	{
 		if((sizeof...(args) % 3))
 			throw std::invalid_argument("the number of arguments in PushEdge() is wrong!");
-		PushEdge(rnfrom_id, rnto_id, rnval);
-		return PushEdge(args...);
+		return PushEdge(rnfrom_id, rnto_id, rnval)&&PushEdge(args...);
+	}
+	bool PushEdgeU(const ID_T& rnid1, const ID_T& rnid2, const Value_T& rnval)
+	{
+		return (PushEdge(rnid1, rnid2, rnval))&&(PushEdge(rnid2, rnid1, rnval));
+	}
+	template<typename...Args>
+	bool PushEdgeU(const ID_T& rnid1, const ID_T& rnid2, const Value_T& rnval, const Args&...args)
+	{
+		if((sizeof...(args) % 3))
+			throw std::invalid_argument("the number of arguments in PushEdge() is wrong!");
+		return PushEdgeU(rnid1, rnid2, rnval)&&PushEdgeU(args...);
 	}
 	bool PushEdge(const rnedge<Value_T, ID_T>& rne)
 	{
@@ -249,8 +259,16 @@ std::cout<<"from:"<<rnfrom<<" to:"<<rnto<<std::endl;
 	template<typename...Args>
 	bool PushEdge(const rnedge<Value_T, ID_T>& rne, const Args&...args)
 	{
-		PushEdge(rne);
-		return PushEdge(args...);
+		return PushEdge(rne)&&PushEdge(args...);
+	}
+	bool PushEdgeU(const rnedge<Value_T, ID_T>& rne)
+	{
+		return PushEdgeU(rne.from, rne.to, rne.weight);
+	}
+	template<typename...Args>
+	bool PushEdgeU(const rnedge<Value_T, ID_T>& rne, const Args...args)
+	{
+		return PushEdgeU(rne)&&PushEdgeU(args...);	
 	}
 //net information
 	bool IsEdge(const ID_T& rnfrom_id, const ID_T& rnto_id) const
@@ -273,9 +291,17 @@ std::cout<<"from:"<<rnfrom<<" to:"<<rnto<<std::endl;
 	{
 		return get_pos(rnid) != NULL_POS;
 	}
+	bool Empty() const
+	{
+		return size() == 0;
+	}
 	size_type size() const
 	{
 		return graph.size();
+	}
+	size_type Edges() const 
+	{
+		return edges;
 	}
 	Value_T GetWeight(const ID_T& rnfrom_id, const ID_T& rnto_id) const
 	{
@@ -295,11 +321,100 @@ std::cout<<"from:"<<rnfrom<<" to:"<<rnto<<std::endl;
 	{
 		return GetWeight(rne.from, rne.to);
 	}
+	void Display(std::ostream& rnos = std::cout) const  //display edges of directed graph
+	{										//print rnwide edges every line and print same orgin vertex every line 
+		std::string rnlink = "->";
+		size_type rnwide = 10;
+		size_type rnsize = size();
+		for(size_type rnpos = 0; rnpos < rnsize; ++rnpos)
+		{
+			auto rnpair = get_neighbor_iter_pair(rnpos);
+			auto rnbegin = rnpair.first;
+			auto rnend = rnpair.second;
+			size_type rncnt = rnwide - 1;
+			auto rnpos_ID = get_ID(rnpos);
+			if(rnbegin != rnend)
+				rnos<<rnpos_ID<<rnlink<<get_ID(rnbegin->first);
+			++rnbegin;
+			while(rnbegin != rnend)
+			{
+				if(!rncnt)
+				{
+					rncnt = rnwide;
+					rnos<<std::endl;
+				}
+				else
+					rnos<<" ";
+				rnos<<rnpos_ID<<rnlink<<get_ID(rnbegin->first);
+				--rncnt;
+				++rnbegin;
+			}
+			rnos<<std::endl;
+		}
+	}
+	void DisplayU(std::ostream& rnos = std::cout) const  //display edges of undirected graph
+	{										 //print rnwide edges every line and print same orgin vertex every line 
+		std::string rnlink = "--";
+		size_type rnwide = 10;
+		size_type rnsize = size();
+		std::unordered_set<std::string> rnmark_uset;
+		for(size_type rnpos = 0; rnpos < rnsize; ++rnpos)
+		{
+			auto rnpair = get_neighbor_iter_pair(rnpos);
+			auto rnbegin = rnpair.first;
+			auto rnend = rnpair.second;
+			size_type rncnt = rnwide;
+			auto rnpos_ID = get_ID(rnpos);
+			while(rnbegin != rnend)
+			{
+				std::string rnkey = std::to_string(rnpos) + "to" + std::to_string(rnbegin->first);
+				if(rnmark_uset.find(rnkey) == rnmark_uset.end())
+				{
+					rnos<<rnpos_ID<<rnlink<<get_ID(rnbegin->first);
+					--rncnt;
+					rnmark_uset.insert(rnkey);
+					std::string rnkey2 = std::to_string(rnbegin->first) + "to" + std::to_string(rnpos); 
+					rnmark_uset.insert(rnkey2);
+					++rnbegin;
+					break;
+				}
+				++rnbegin;
+			}
+			while(rnbegin != rnend)
+			{
+			
+				std::string rnkey = std::to_string(rnpos) + "to" + std::to_string(rnbegin->first);
+				if(rnmark_uset.find(rnkey) == rnmark_uset.end())
+				{
+					if(!rncnt)
+					{
+						rncnt = rnwide;
+						rnos<<std::endl;
+					}
+					else 
+					{
+						rnos<<" ";
+					}
+					rnos<<rnpos_ID<<rnlink<<get_ID(rnbegin->first);
+					--rncnt;
+					rnmark_uset.insert(rnkey);
+					std::string rnkey2 = std::to_string(rnbegin->first) + "to" + std::to_string(rnpos); 
+					rnmark_uset.insert(rnkey2);
+				}
+				++rnbegin;
+			}
+			rnos<<std::endl;
+		}
+	}
+
+
 //important methods
-	rnpath<Value_T, ID_T> rnDijkstra(const ID_T&, const ID_T&) const;
+	rnpath<Value_T, ID_T> rnDijkstra(const ID_T&, const ID_T&) const; // Shortest path algorithem 
+	std::pair<rngraph<Value_T, ID_T>, Value_T> rnPrim(const ID_T&) const; // minimal spanning tree
+	std::pair<rngraph<Value_T, ID_T>, Value_T> rnPrim() const;
 	std::pair<bool, ID_T*> DFS(ID_T& rnorgin, std::function<bool(ID_T&)> rnvisit) //depth-first search  one-place predicate bool(ID_t&)
 	{
-		if(size() == 0)
+		if(Empty())
 			return {false, &(rnorgin)};
 		std::vector<bool> rnrecord_vec(graph.size(), false); //visited table
 		auto rnorgin_pos = get_pos(rnorgin);
@@ -326,6 +441,24 @@ std::cout<<"from:"<<rnfrom<<" to:"<<rnto<<std::endl;
 	//template<typename...Args>
 	//void push_vertex(size_type, size_type, const Value_T&, const Args&...args);  //mutable parameter function template
 private:
+	std::shared_ptr<std::vector<std::vector<std::pair<Value_T, size_type>>>> adj_lst2adj_mat() const //convert data structure 
+	{																								 //from adjacent list to adjacent matrix
+		std::vector<std::pair<Value_T, size_type>> rnrow(size(), {Value_T(), 0});
+		auto rnret_ptr = std::make_shared<std::vector<std::vector<std::pair<Value_T, size_type>>>>(size(), rnrow);
+		for(size_type rnrow_n = 0; rnrow_n < size(); ++rnrow_n)
+		{
+			auto rnpair = get_neighbor_iter_pair(rnrow_n);
+			auto rnbegin = rnpair.first;
+			auto rnend = rnpair.second;
+			while(rnbegin != rnend)
+			{
+				((*rnret_ptr)[rnrow_n][rnbegin->first]).first = rnbegin->second;
+				((*rnret_ptr)[rnrow_n][rnbegin->first]).second += 1;
+				++rnbegin;
+			}
+		}
+		return rnret_ptr;
+	}
 	std::shared_ptr<std::vector<std::pair<size_type, Value_T>>> get_neighbor(const ID_T& rnid) const
 	{
 		auto rnpos = get_pos(rnid);
@@ -360,22 +493,30 @@ private:
 		else
 			pos_vec.push_back({rnid, rnpos});
 	}
-	std::pair<typename std::vector<std::pair<size_type, Value_T>>::iterator, typename std::vector<std::pair<size_type, Value_T>>::iterator> get_neighbor_iter_pair(const size_type& rnpos)
+	std::pair<typename std::vector<std::pair<size_type, Value_T>>::iterator, 
+		typename std::vector<std::pair<size_type, Value_T>>::iterator> get_neighbor_iter_pair(const size_type& rnpos)
 	{
 		return {(graph[rnpos].second)->begin(), (graph[rnpos].second)->end()};
 	}
+
+	std::pair<typename std::vector<std::pair<size_type, Value_T>>::const_iterator,
+		typename std::vector<std::pair<size_type, Value_T>>::const_iterator> get_neighbor_iter_pair(const size_type& rnpos) const
+	{
+		return {(graph[rnpos].second)->cbegin(), (graph[rnpos].second)->cend()};
+	}
+
 	std::pair<bool, ID_T*> dfs_recursion(const size_type& rnorgin, std::function<bool(ID_T&)> rnvisit, std::vector<bool>& rnrecord_vec);  //depth-first
+//data member
 	std::vector<std::pair<ID_T, std::shared_ptr<std::vector<std::pair<size_type, Value_T>>>>> graph; 
 	std::vector<std::pair<ID_T, size_type>> pos_vec;
 	std::map<ID_T, size_type> pos_map;
 	size_type edges = 0;
-
 };
 
 /// static member initialization
 template<typename Value_T, typename ID_T>
 bool rngraph<Value_T, ID_T>::support_less_operator = false;  //initialize the signal
-///
+
 ///define member function
 template<typename Value_T, typename ID_T>
 std::pair<bool, ID_T*> rngraph<Value_T, ID_T>::dfs_recursion
@@ -406,7 +547,7 @@ template<typename Value_T, typename ID_T>
 std::pair<bool, ID_T*> rngraph<Value_T, ID_T>::BFS
 			(ID_T& rnorgin, std::function<bool(ID_T&)> rnvisit)
 {
-	if(size() == 0)
+	if(Empty())
 		return {false, &(rnorgin)};
 	std::vector<bool> rnrecord_vec(graph.size(), false); //visited table
 	std::queue<size_type> rnq;
@@ -554,6 +695,62 @@ rnpath<Value_T, ID_T> rngraph<Value_T, ID_T>::rnDijkstra(const ID_T& rnorgin, co
 	}
 	rnpath<Value_T, ID_T> rnpath_ret(rnpath_fl_p, rntotal_weight);
 	return rnpath_ret;
+}
+template<typename Value_T, typename ID_T>
+std::pair<rngraph<Value_T, ID_T>, Value_T> rngraph<Value_T, ID_T>::rnPrim(const ID_T& rnorgin) const  // minimal spanning tree
+{
+	auto rnorgin_pos = get_pos(rnorgin);
+	if(Empty()||rnorgin_pos == NULL_POS)
+		throw std::runtime_error("Empty graph or Wrong vertex ID!");
+	std::priority_queue<std::tuple<rnposition_type, rnposition_type, Value_T>, std::vector<std::tuple<rnposition_type, rnposition_type, Value_T>>,
+								decltype(rncompare3<Value_T, ID_T>)*> rnpq(rncompare3<Value_T, ID_T>);
+	std::vector<bool> rnmark_vec(size(), false);    //mark set
+	auto rnpair = get_neighbor_iter_pair(rnorgin_pos);
+	auto rnbegin_orgin = rnpair.first;
+	auto rnend_orgin = rnpair.second;
+	rnmark_vec[rnorgin_pos] = true;
+	while(rnbegin_orgin != rnend_orgin)
+	{
+		rnpq.push(std::make_tuple(rnorgin_pos, rnbegin_orgin->first, rnbegin_orgin->second));
+		++rnbegin_orgin;
+	}
+	size_type rncnt = 0;  // the number of edges in the mst
+	auto rnsize = size(); // the number of vertex in the graph
+	std::pair<rngraph<Value_T, ID_T>, Value_T> rnret_pair;    // <mst, weight_sum>
+	while((rncnt != rnsize - 1)&&rnpq.size())
+	{
+		auto rnnext_tuple = rnpq.top();
+		rnpq.pop();
+		if(rnmark_vec[std::get<1>(rnnext_tuple)])
+			continue;
+		rnret_pair.first.PushEdgeU(get_ID(std::get<0>(rnnext_tuple)), get_ID(std::get<1>(rnnext_tuple)),
+				std::get<2>(rnnext_tuple)); //add edge into mst
+		rnret_pair.second += std::get<2>(rnnext_tuple);    // record the weight;
+		rnmark_vec[std::get<1>(rnnext_tuple)] = true;      // mark the vertex
+		++rncnt;
+
+		auto rnpair = get_neighbor_iter_pair(std::get<1>(rnnext_tuple));
+		auto rnbegin = rnpair.first;
+		auto rnend = rnpair.second;
+		while(rnbegin != rnend)                           // add neighbor edges into priority queue
+		{
+			if(rnmark_vec[rnbegin->first])
+			{
+				++rnbegin;
+				continue;
+			}
+			rnpq.push(std::make_tuple(std::get<1>(rnnext_tuple), rnbegin->first, rnbegin->second));
+			++rnbegin;
+		}
+	}
+	return std::move(rnret_pair);
+}
+template<typename Value_T, typename ID_T>
+std::pair<rngraph<Value_T, ID_T>, Value_T> rngraph<Value_T, ID_T>::rnPrim() const
+{
+	if(Empty())
+		return std::pair<rngraph<Value_T, ID_T>, Value_T>();
+	return rnPrim(get_ID(0));
 }
 
 ///
